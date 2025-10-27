0ba60932c7e307a0ca8f07c79881516270a14738
krunalbrahmankar11 <krunalbrahmankar43@gmail.com>
2025-10-27 10:24:40 +0530

    update

--- Project/SmartShelfX_AI_Based_Invetory/src/pages/Inventory.jsx
+++ Project/SmartShelfX_AI_Based_Invetory/src/pages/Inventory.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Inventory.css";
+import './Inventory.css';
 
 export default function Inventory() {
   const [projects, setProjects] = useState([]);
